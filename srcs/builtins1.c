6d9c3feb4f0cce3848fda7ae738e7731b100f88c
Philippe Menard <phil.menard1668@gmail.com>
2025-02-25 13:41:23 +0100

    start to work on echo, push to retrieve Leon's code

--- srcs/builtins1.c
+++ srcs/builtins1.c
@@ -25,3 +25,33 @@ void	ft_env(char **env)
 		i++;
 	}
 }
+
+void	ft_echo(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '$')
+		{
+
+		}
+		else
+		{
+			write(1, &str[i], 1);
+			i++;
+		}
+	}
+}
+
+/*
+// Name of the environment variable (e.g., PATH)
+   const char *name = "PATH";
+   // Get the value associated with the variable
+   const char *env_p = getenv(name);
+   if(env_p){
+      printf("Your %s is %s\n", name, env_p);
+   }
+   return 0;
+   */
