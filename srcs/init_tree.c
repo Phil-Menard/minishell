2a714910363f5182ff800fe51e1caf7c0d20acf3
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-03-03 13:03:43 +0100

    modified struct, ast->binary tree

--- srcs/init_tree.c
+++ srcs/init_tree.c
@@ -1,47 +1,6 @@
 # include "minishell.h"
 
-void	ft_free_list(t_tree *tree)
-{
-	if (!tree)
-		return ;
-	ft_free_list(tree->left);
-	ft_free_list(tree->right);
-	free(tree);
-}
-
-t_tree	*ft_new_node(char *cmd)
-{
-	t_tree	*node;
-
-	node = malloc(sizeof(t_tree));
-	if (!node)
-		return (NULL);
-	node->left = NULL;
-	node->right = NULL;
-	node->cmd = cmd;
-	return (node);
-}
-
-void	ft_add_node_end(t_tree *tree, const char *way, char *cmd)
-{
-	t_tree	*cur;
-
-	cur = tree;
-	if (ft_strcmp(way, "left") == 0)
-	{
-		while (cur->left != NULL)
-			cur = cur->left;
-		cur->left = ft_new_node(cmd);
-	}
-	else if (ft_strcmp(way, "right") == 0)
-	{
-		while (cur->right != NULL)
-			cur = cur->right;
-		cur->right = ft_new_node(cmd);
-	}
-}
-
-//! For testing
+//! For testing, a retirer a la fin
 void ft_print_tree(t_tree *root, int space)
 {
 	int i;
@@ -64,4 +23,68 @@ void ft_print_tree(t_tree *root, int space)
 
 	// Afficher ensuite le sous-arbre gauche
 	ft_print_tree(root->left, space);
+}
+
+// recursively free the tree
+void	ft_free_list(t_tree *tree)
+{
+	if (!tree)
+		return ;
+	ft_free_list(tree->left);
+	ft_free_list(tree->right);
+	free(tree);
+}
+
+// create a node, set the next left and right branch to null
+t_tree	*ft_new_node(char *cmd)
+{
+	t_tree	*node;
+
+	node = malloc(sizeof(t_tree));
+	if (!node)
+		return (NULL);
+	node->left = NULL;
+	node->right = NULL;
+	node->cmd = cmd;
+	return (node);
+}
+
+// add a node at the end of the branch
+void	ft_add_node_end(t_tree *tree, const char *branch, char *cmd)
+{
+	t_tree	*cur;
+
+	cur = tree;
+	if (ft_strcmp(branch, "left") == 0)
+	{
+		while (cur->left != NULL)
+			cur = cur->left;
+		cur->left = ft_new_node(cmd);
+	}
+	else if (ft_strcmp(branch, "right") == 0)
+	{
+		while (cur->right != NULL)
+			cur = cur->right;
+		cur->right = ft_new_node(cmd);
+	}
+}
+
+/*	Split each words of line (separate by space)
+	Parse the split tab, if a word is an executable (check with access() == 0),
+		create a branch
+*/
+void	ft_create_branch(char *line)
+{
+	char	**split;
+	int		i_arg;
+	t_tree	*tree;
+
+
+	split = ft_split(line, ' ');
+	i_arg = 0;
+	while (split[i_arg])
+	{
+		if (access(split[i_arg], X_OK) == 0)
+
+	}
 }
--- srcs/minishell.h
+++ srcs/minishell.h
@@ -13,6 +13,8 @@
 typedef struct s_tree
 {
 	char			*cmd;
+	char			*option;
+	char			*arg;
 	struct s_tree	*left;
 	struct s_tree	*right;
 }					t_tree;
