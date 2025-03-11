f47e3e1ffce05cf8e9c725c804860dbe1a4b0694
lefoffan <lefoffan@student.42perpignan.fr>
2025-03-11 23:57:26 +0100

    Tokenizer work ! It did't work bcaus i'm stupid

--- libft/ft_strndup.c
+++ libft/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: lefoffan <lefoffan@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 11:51:34 by lefoffan          #+#    #+#             */
-/*   Updated: 2025/03/07 13:36:01 by lefoffan         ###   ########.fr       */
+/*   Updated: 2025/03/11 23:26:33 by lefoffan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 char	*ft_strndup(const char *s, size_t start, size_t len)
 {
 	char	*dst;
+	size_t	i;
 
-	if (len > ft_strlen(s))
+	if (!s || start >= ft_strlen(s))
 		return (NULL);
 	dst = ft_calloc((len + 1), sizeof(char));
 	if (!dst)
 		return (NULL);
-	while (start < len && s[start])
+	i = 0;
+	while (i < len && s[start + i])
 	{
-		dst[start] = s[start];
-		start++;
+		dst[i] = s[start + i];
+		i++;
 	}
-	dst[start] = '\0';
+	dst[i] = '\0';
 	return (dst);
 }
--- srcs/parsing/lexing.c
+++ srcs/parsing/lexing.c
@@ -1,5 +1,4 @@
 #include "../minishell.h"
-void	ft_addchar_to_token(t_token_builder **builder, char c);
 
 //* Add all quoted text in buf into list token_builder
 void	ft_addquotes_to_token(t_token_builder **builder, char *line, int start)
@@ -9,20 +8,13 @@ void	ft_addquotes_to_token(t_token_builder **builder, char *line, int start)
 
 	if (!line)
 		return ;
-
-	if (line[start] == '\'')
+	if (line[start++] == '\'')
 		size = ft_get_pos(line, start, '\'') - start;
 	else if (line[start] == '\"')
 		size = ft_get_pos(line, start, '\"') - start;
-
-
 	last = ft_get_last(*builder);
 	last->next = ft_new_tkb(size, ft_strndup(line, start, size));
-	printf("addquote : %s\n", last->next->buf);
 }
-
-
-
 
 //* Add char by char to buf into list token_builder
 void	ft_addchar_to_token(t_token_builder **builder, char c)
@@ -31,36 +23,23 @@ void	ft_addchar_to_token(t_token_builder **builder, char c)
 
 	if (!*builder)
 	{
-		*builder = ft_calloc(sizeof(t_token_builder), 1);
-		if (!*builder)
-			return ;
-		(*builder)->len = 1;
-		(*builder)->buf = ft_calloc(sizeof(char), LEX_BUFF_SIZE);
+		*builder = ft_new_tkb(1, ft_calloc(sizeof(char), LEX_BUFF_SIZE));
 		(*builder)->buf[0] = c;
-		(*builder)->next = NULL;
 		return ;
 	}
 	last = ft_get_last(*builder);
 	if (last->len >= LEX_BUFF_SIZE)
 		last->next = ft_new_tkb(0, ft_calloc(sizeof(char), LEX_BUFF_SIZE));
 	last->buf[last->len++] = c;
-	printf("addchar : %s\n", last->buf);
 }
 
-
-
-
-
-
 //* State machine
-//		* Normal : add char by char to buf
-//		* Quotes : take all btw quotes and add it to buf
+//		* Normal : add char by char to a token
+//		* Quotes : take all btw quotes and add it to a new token
 t_token_builder	*ft_tokenizer(char *line)
 {
 	t_token_builder	*tokens;
 	int				i;
-
-
 
 	tokens = NULL;
 	i = -1;
@@ -70,30 +49,34 @@ t_token_builder	*ft_tokenizer(char *line)
 			&& (ft_strfind(line + i, "\'") || ft_strfind(line + i, "\"")))
 		{
 			ft_addquotes_to_token(&tokens, line, i);
-			i += ft_strlen(ft_get_last(tokens)->buf);
+			i += ft_strlen(ft_get_last(tokens)->buf) + 1;
+			if (line[i + 1] != '\0')
+				ft_get_last(tokens)->next = ft_new_tkb(0, ft_calloc(sizeof(char), LEX_BUFF_SIZE));
 		}
 		else
 			ft_addchar_to_token(&tokens, line[i]);
 	}
-
 	return (tokens);
 }
 
+/** 
+ * todo : plus qu'a concat les tokens comme il faut -> detecter le type du token (cmd, arg, pipe, and, or ?)
+ * todo : les mettres dans des nodes
+ * todo : s'occuper de la creation de l'ast pour mettre les nodes aux bons endroits
+ * */
 
+//-------------------------TESTS---------------------------------
 
+// int	main(void)
+// {
+// 	char			*test = "echo 'hello world' peach \"!\"";
+// 	t_token_builder	*tokens = ft_tokenizer(test);
 
-//!-------------------------TESTS---------------------------------
-
-int main()
-{
-	char *test = "echo 'hello world'";
-	t_token_builder *tokens = ft_tokenizer(test);
-
-	printf("str : %s\n", test);
-	while (tokens)
-	{
-		printf("Token: %s\n", tokens->buf);
-		tokens = tokens->next;
-	}
-	return 0;
-}
+// 	printf("str : %s\n", test);
+// 	while (tokens)
+// 	{
+// 		printf("Token: %s\n", tokens->buf);
+// 		tokens = tokens->next;
+// 	}
+// 	return 0;
+// }
