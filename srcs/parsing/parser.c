4a39e4d53352b28bb09045f545d497002c41abee
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-03-13 19:03:56 +0100

    jsp

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -1,6 +1,5 @@
 # include "../minishell.h"
 
-//* func called by main, call all other
 void	ft_parse(char *line, t_env *env)
 {
 	t_ast			*tree;
@@ -13,42 +12,13 @@ void	ft_parse(char *line, t_env *env)
 		printf("Error odd nb of quotes.\n");
 		return ;
 	}
-	tokens = ft_tokenizer(ft_prep_tok(line)); // lexing
+	tokens = ft_tokenizer(line); // lexing
 	// create tree
 	// exec
 	ft_free_list(tree);
 }
 
-
-
-
-//* Join les bouts de split pour reconstituer la ligne de commande qui sera lance avec execve ou nos builtins
-// char	*ft_get_cmd(char **split, int *start)
-// {
-// 	char	*str;
-
-// 	if (ft_get_token(split[*start]) == CMD)
-// 	{
-// 		while (ft_get_token(split[*start]) == CMD)
-// 		{
-// 			str = ft_straddstr(str, split[*start]);
-// 			if (split[*start + 1])
-// 				str = ft_straddchar(str, ' ');
-// 			(*start)++;
-// 		}
-
-// 	}
-// 	return (str);
-// }
-//* update : peut etre inutile au finale... 
-
-
-
-
-//* Create the AST recursively
 void	ft_create_tree(t_ast *tree, char **split, int i_arg)
 {
 
-	free_db_array(split);
-	split = NULL;
 }
