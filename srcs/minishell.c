f822bbfd504eb4d136a88e90c8cda7955dc3967d
Philippe Menard <phil.menard1668@gmail.com>
2025-04-07 16:57:56 +0200

    fixed when there is no cmd

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -18,11 +18,11 @@ void	check_pipes(t_var *vars, t_env **env, t_env **export)
 {
 	int		*fd;
 
-	fd = NULL;	
+	fd = NULL;
 	if (!vars->tokens)
 		return ;
 	fd = init_and_set_fd(vars->cmd_line, vars, env);
-	if (vars->nb_cmd_line == 1 && fd[0] > -1)
+	if (vars->cmd_line[0].cmd && vars->nb_cmd_line == 1 && fd[0] > -1)
 	{
 		vars->i = 0;
 		builtin_or_cmd(vars, fd, env, export);
